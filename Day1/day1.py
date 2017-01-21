48a6ec17dfdddc3e61544be82f0c53f6d37f6c09
KASIENKA <kgarbulewska@gmail.com>
2017-01-21 12:44:34 +0100

    day1

---
+++ Day1/day1.py
@@ -0,0 +1,29 @@
+import sys
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+
+# Declare second integer, double, and String variables.
+i2 = 12
+d2 = 4.0
+s2 = 'is the best place to learn and practice coding!'
+
+# Read and save an integer, double, and String to your variables.
+
+i2 = int(raw_input())
+d2 = float(raw_input())
+s2 = str(raw_input())
+
+# Print the sum of both integer variables on a new line.
+print i + i2
+
+# Print the sum of the double variables on a new line.
+print d + d2
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+print s + s2
+
