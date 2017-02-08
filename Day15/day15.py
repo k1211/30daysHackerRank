befd703f72360678a293c804ffdb81456c23d6ff
KASIENKA <kgarbulewska@gmail.com>
2017-02-08 11:50:20 +0100

    day15

--- Day15/day15.py
+++ Day15/day15.py
@@ -8,6 +8,7 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
+
 class Solution:
     def display(self,head):
         current = head
@@ -29,10 +30,10 @@ class Solution:
                     current = current.next
             return head
 
-mylist= Solution()
-T=int(input())
-head=None
+mylist = Solution()
+T = int(input())
+head = None
 for i in range(T):
-    data=int(input())
-    head=mylist.insert(head,data)
-mylist.display(head);
+    data = int(input())
+    head = mylist.insert(head,data)
+mylist.display(head)
